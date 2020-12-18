bd9b556cf982b11c5b80294ab6c64a68754f5123
simonchen92 <simon.k.chen92@gmail.com>
2020-12-18 12:07:05 -0500

    code war solution to mumbling problem

---
+++ code-practice/codewars/mumbling.js
@@ -0,0 +1,57 @@
+/*
+This time no story, no theory. The examples below show you how to write function accum:
+
+Examples:
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+
+The parameter of accum is a string which includes only letters from a..z and A..Z.
+
+Direct Link
+https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/javascript
+*/
+
+// Solution
+
+/*
+accum("abcd") -> "A-Bb-Ccc-Dddd"
+accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
+accum("cwAt") -> "C-Ww-Aaa-Tttt"
+*/
+
+// Solution 1
+// Using for loop
+function accum(s) {
+  let arr = [];
+  let lowerStr = s.toLowerCase();
+
+  for (let i = 0; i < lowerStr.length; i++) {
+    let tempStr = lowerStr[i].toUpperCase();
+    for (let j = 0; j < i; j++) {
+      tempStr += lowerStr[i];
+    }
+    arr.push(tempStr);
+  }
+  return arr.join('-');
+}
+
+console.log(accum('ZpglnRxqenU')); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum('NyffsGeyylB')); // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum('MjtkuBovqrU')); // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum('EvidjUnokmM')); // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum('HbideVbxncC')); // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
+
+// Solution 2
+// Using map
+function accum1(s) {
+  return [...s]
+    .map((char, index) => char.toUpperCase() + char.toLowerCase().repeat(index))
+    .join('-');
+}
+
+console.log(accum1('ZpglnRxqe1nU')); // "Z-Pp-Ggg-Llll-Nnnnn-Rrrrrr-Xxxxxxx-Qqqqqqqq-Eeeeeeeee-Nnnnnnnnnn-Uuuuuuuuuuu"
+console.log(accum1('NyffsGeyylB')); // "N-Yy-Fff-Ffff-Sssss-Gggggg-Eeeeeee-Yyyyyyyy-Yyyyyyyyy-Llllllllll-Bbbbbbbbbbb"
+console.log(accum1('MjtkuBovqrU')); // "M-Jj-Ttt-Kkkk-Uuuuu-Bbbbbb-Ooooooo-Vvvvvvvv-Qqqqqqqqq-Rrrrrrrrrr-Uuuuuuuuuuu"
+console.log(accum1('EvidjUnokmM')); // "E-Vv-Iii-Dddd-Jjjjj-Uuuuuu-Nnnnnnn-Oooooooo-Kkkkkkkkk-Mmmmmmmmmm-Mmmmmmmmmmm"
+console.log(accum1('HbideVbxncC')); // "H-Bb-Iii-Dddd-Eeeee-Vvvvvv-Bbbbbbb-Xxxxxxxx-Nnnnnnnnn-Cccccccccc-Ccccccccccc"
