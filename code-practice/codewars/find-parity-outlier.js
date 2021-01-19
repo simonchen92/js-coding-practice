b8554b8e98262d84f973583428ddd716c0b0c4ee
simonchen92 <simon.k.chen92@gmail.com>
2021-01-19 11:50:48 -0500

    commented back in some console.log

--- code-practice/codewars/find-parity-outlier.js
+++ code-practice/codewars/find-parity-outlier.js
@@ -61,11 +61,11 @@ function findOutlier(integers) {
   }
 }
 
-// console.log(findOutlier([0, 1, 2])); //  1
-// console.log(findOutlier([1, 2, 3])); // 2
-// console.log(findOutlier([2, 6, 8, 10, 3])); // 3
-// console.log(findOutlier([0, 0, 3, 0, 0])); // 3
-// console.log(findOutlier([1, 1, 0, 1, 1])); // 0
+console.log(findOutlier([0, 1, 2])); //  1
+console.log(findOutlier([1, 2, 3])); // 2
+console.log(findOutlier([2, 6, 8, 10, 3])); // 3
+console.log(findOutlier([0, 0, 3, 0, 0])); // 3
+console.log(findOutlier([1, 1, 0, 1, 1])); // 0
 
 // Solution 2
 // Using filter
