5e0cde2f4bd60926faf90e2aa403bdbfc8decb2a
simonchen92 <simon.k.chen92@gmail.com>
2021-02-01 15:57:10 -0500

    codewar solution to find missing letter in array

---
+++ code-practice/codewars/find-missing-letter.js
@@ -0,0 +1,71 @@
+/*
+Find the missing letter
+
+Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+The array will always contain letters in only one case.
+
+Example:
+['a','b','c','d','f'] -> 'e' 
+['O','Q','R','S'] -> 'P'
+
+(Use the English alphabet with 26 letters!)
+
+Direct Link
+https://www.codewars.com/kata/5839edaa6754d6fec10000a2/train/javascript
+*/
+
+// Solution
+function findMissingLetter(array) {
+  // manually added alphabets
+  let alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  let firstLetter = array[0];
+
+  if (firstLetter === firstLetter.toUpperCase()) {
+    alphabet = alphabet.join('').toUpperCase().split('');
+  }
+
+  // get first array element
+  let firstArrayElement = alphabet.indexOf(firstLetter);
+
+  // get the range of element in our array
+  alphabet = alphabet.splice(firstArrayElement, array.length + 1);
+
+  // loop through alphabet and if the array doesn't have that alphabet we will return it
+  for (let i = 0; i < alphabet.length + 1; i++) {
+    if (array.indexOf(alphabet[i]) === -1) {
+      return alphabet[i];
+    }
+  }
+}
+
+console.log(findMissingLetter(['a', 'b', 'c', 'd', 'f'])); // 'e'
+console.log(findMissingLetter(['O', 'Q', 'R', 'S'])); // 'P'
