285ac4e2a390561f4fc09db0258209bc397b75ac
simonchen92 <simon.k.chen92@gmail.com>
2021-06-09 15:41:02 -0400

    added alternate js solution

--- code-practice/codewars/Higher-Order-Function-16.js
+++ code-practice/codewars/Higher-Order-Function-16.js
@@ -32,21 +32,85 @@
 
 // Test Case
 const list1 = [
-  { firstName: null, lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: null },
-  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
-]
+  {
+    firstName: null,
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: null,
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+];
 
 const list2 = [
-  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
-]
+  {
+    firstName: 'Sofia',
+    lastName: 'I.',
+    country: 'Argentina',
+    continent: 'Americas',
+    age: 35,
+    language: 'Java',
+  },
+  {
+    firstName: 'Lukas',
+    lastName: 'X.',
+    country: 'Croatia',
+    continent: 'Europe',
+    age: 35,
+    language: 'Python',
+  },
+  {
+    firstName: 'Madison',
+    lastName: 'U.',
+    country: 'United States',
+    continent: 'Americas',
+    age: 32,
+    language: 'Ruby',
+  },
+];
 
 // Solution
 
-const askForMissingDetails = list =>
-  list.filter(dev => Object.keys(dev).some(key => dev[key] === null && (dev.question = `Hi, could you please provide your ${key}.`)))
+const askForMissingDetails = (list) =>
+  list.filter((dev) =>
+    Object.keys(dev).some(
+      (key) =>
+        dev[key] === null &&
+        (dev.question = `Hi, could you please provide your ${key}.`),
+    ),
+  );
 
-console.log(askForMissingDetails(list1))
-console.log(askForMissingDetails(list2))
+console.log(askForMissingDetails(list1));
+console.log(askForMissingDetails(list2));
+
+// Solution 2
+
+function askForMissingDetails2(list) {
+  return list.filter((dev) => {
+    for (let key in dev) {
+      if (dev[key] === null) {
+        dev['question'] = `Hi, could you please provide your ${key}.`;
+        return dev;
+      }
+    }
+  });
+}
+
+console.log(askForMissingDetails2(list1));
+console.log(askForMissingDetails2(list2));
