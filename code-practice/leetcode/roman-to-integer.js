7065071fa77c38f01d05ef0a10e47eca87b1c8eb
simonchen92 <simon.k.chen92@gmail.com>
2021-10-12 14:05:16 -0400

    javascript solution to roman to integer

---
+++ code-practice/leetcode/roman-to-integer.js
@@ -0,0 +1,81 @@
+/*
+For example, 2 is written as II in Roman numeral, just two one's added together. 12 is written as XII, which is simply X + II. 
+The number 27 is written as XXVII, which is XX + V + II.
+
+Roman numerals are usually written largest to smallest from left to right. However, the numeral for four is not IIII. 
+Instead, the number four is written as IV. Because the one is before the five we subtract it making four. T
+he same principle applies to the number nine, which is written as IX. There are six instances where subtraction is used:
+
+I can be placed before V (5) and X (10) to make 4 and 9. 
+X can be placed before L (50) and C (100) to make 40 and 90. 
+C can be placed before D (500) and M (1000) to make 400 and 900.
+Given a roman numeral, convert it to an integer.
+
+
+Example 1:
+Input: s = "III"
+Output: 3
+
+Example 2:
+Input: s = "IV"
+Output: 4
+
+Example 3:
+
+Input: s = "IX"
+Output: 9
+
+Example 4:
+Input: s = "LVIII"
+Output: 58
+Explanation: L = 50, V= 5, III = 3.
+
+Example 5:
+Input: s = "MCMXCIV"
+Output: 1994
+Explanation: M = 1000, CM = 900, XC = 90 and IV = 4.
+ 
+
+Constraints:
+1 <= s.length <= 15
+s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
+It is guaranteed that s is a valid roman numeral in the range [1, 3999].
+
+Direct Link
+https://leetcode.com/problems/roman-to-integer/
+*/
+
+// Solution
+
+const romanToInt = function(s) {
+  // create a obj to store all the key value pairs
+  const obj = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  };
+
+  // create value variable to store the num value
+  let value = 0;
+
+  for (let i = 0; i < s.length; i++) {
+    // make a curr pointer and next pointer and compare them
+    let curr = obj[s[i]];
+    let next = obj[s[i + 1]];
+    // if curr pointer is less than the next pointer then decrease the num value
+    if (curr < next) value -= curr;
+    // if curr pointer is greater or equal to the next pointer then increase num value
+    else value += curr;
+  }
+  return value;
+};
+
+console.log(romanToInt('III')); // 3
+console.log(romanToInt('IV')); // 4
+console.log(romanToInt('IX')); // 9
+console.log(romanToInt('LVIII')); // 58
+console.log(romanToInt('MCMXCIV')); // 1994
